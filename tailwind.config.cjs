f3d7d2ac1d0ab96a5ed348e62565dd6f942ce7e8
yai <arsenovlidija02@gmail.com>
2023-02-06 19:38:48 +0100

    Homepage and products page init

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require("@tailwindcss/forms"),
-		require("@tailwindcss/line-clamp"),
-		require("@tailwindcss/aspect-ratio"),
-		require("@tailwindcss/typography"),
-	],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFC639",
+        secondary: "#881226",
+        info: "#F13D9E",
+        sky: "#32B7E8",
+        dark: "#151515",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
 }
