4605faf78451e401926183909bbcfb8135bcfa4e
Michael Bouclas <mbouclas@gmail.com>
2023-11-16 11:26:05 +0200

    A million fixes

--- src/helpers/cloudinary.helper.ts
+++ src/helpers/cloudinary.helper.ts
@@ -23,18 +23,25 @@ export function optimizeCloudinaryImage(src: string, w: number|null = null, h: n
         src = src['url'];
     }
 
-    const ext = src.split('.').pop()
-    if ([ 'jpg', 'png' ].indexOf(ext as string) !== -1) {
-        src = src.replace(/\.[^/.]+$/, ".webp")
+    try {
+        const ext = src.split('.').pop()
+        if ([ 'jpg', 'png' ].indexOf(ext as string) !== -1) {
+            src = src.replace(/\.[^/.]+$/, ".webp")
+        }
+
+        const width = (w) ? `w_${w},` : 'w_auto,';
+        const height = (h) ? `h_${h},` : '';
+        let crop = (w || h) ? 'c_fit,' : '';
+        if (crop.length > 0) {
+            crop = `c_${cropMode},`
+        }
+        return src.replace('upload/', `upload/${width}${height}${crop}q_${q},f_auto,dpr_auto/`);
+    }
+    catch (e) {
+        console.log('***', src)
+        return  ``;
     }
 
-    const width = (w) ? `w_${w},` : 'w_auto,';
-    const height = (h) ? `h_${h},` : '';
-    let crop = (w || h) ? 'c_fit,' : '';
-    if (crop.length > 0) {
-        crop = `c_${cropMode},`
-    }
-    return src.replace('upload/', `upload/${width}${height}${crop}q_${q},f_auto,dpr_auto/`);
 }
 
 export function cloudinarySrcSet(src: string, sizes = [767, 1190]) {
